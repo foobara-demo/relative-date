adfc8aa1fa29a7dd27665a6c67146903f527bde3
Miles Georgi <azimux@gmail.com>
2025-08-11 15:58:29 -0700

    Make CI pass

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,8 @@
 require "vcr"
 
 VCR.configure do |config|
-  # This is not actually sensitive data but doing this anyway
-  config.filter_sensitive_data("<SCRUBBED_OLLAMA_API_KEY>") { ENV.fetch("OLLAMA_API_KEY", nil) }
+  # This is not actually sensitive data but doing this anyway to make things normalized across environments
+  config.filter_sensitive_data("<SCRUBBED_OLLAMA_API_URL>") { ENV.fetch("OLLAMA_API_URL", nil) }
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
